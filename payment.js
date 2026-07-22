edc480ee0ae9f0d59c4a2d34e0aaa4c4f2eccdb8
missperfecttt <abigailmekonnen70@gmail.com>
2026-07-22 03:01:21 -0700

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,48 +1,158 @@
-function copyNumber(number){
+const paymentForm = document.getElementById("paymentForm");
 
-    navigator.clipboard.writeText(number);
 
-    alert("Copied: " + number);
+const paymentScriptURL =
+"PASTE_YOUR_PAYMENT_APPS_SCRIPT_URL_HERE";
+
+
+const cloudinaryURL =
+"https://api.cloudinary.com/v1_1/xpzpo4yy/image/upload";
+
+
+const uploadPreset =
+"lunascakes_upload";
+
+
+
+paymentForm.addEventListener("submit", async(e)=>{
+
+
+e.preventDefault();
+
+
+
+let screenshotURL="";
+
+
+
+const file =
+document.getElementById("paymentScreenshot")
+.files[0];
+
+
+
+try{
+
+
+if(file){
+
+
+const formData = new FormData();
+
+
+formData.append(
+"file",
+file
+);
+
+
+formData.append(
+"upload_preset",
+uploadPreset
+);
+
+
+
+const upload =
+await fetch(
+cloudinaryURL,
+{
+method:"POST",
+body:formData
+}
+);
+
+
+
+const result =
+await upload.json();
+
+
+
+screenshotURL =
+result.secure_url;
+
 
 }
 
 
 
-const paymentForm = document.getElementById("paymentForm");
+const paymentData = {
 
 
-paymentForm.addEventListener("submit", function(e){
-
-    e.preventDefault();
-
-
-    const name =
-    document.getElementById("customerName").value;
+name:
+document.getElementById("name").value,
 
 
-    const image =
-    document.getElementById("paymentImage").files[0];
+phone:
+document.getElementById("phone").value,
 
 
+paymentMethod:
+document.getElementById("paymentMethod").value,
 
-    if(!image){
 
-        alert("Please upload your payment screenshot");
+screenshotURL:screenshotURL
 
-        return;
 
-    }
+};
 
 
 
-    alert(
-        "Thank you " + name +
-        "! Your payment verification has been received."
-    );
+const response =
+await fetch(
+paymentScriptURL,
+{
+
+method:"POST",
+
+body:
+new URLSearchParams(paymentData)
+
+}
+
+);
 
 
 
-    paymentForm.reset();
+const data =
+await response.json();
+
+
+
+if(data.result==="success"){
+
+
+alert(
+"Payment verification submitted successfully!"
+);
+
+
+paymentForm.reset();
+
+
+}
+else{
+
+alert(
+"Something went wrong"
+);
+
+}
+
+
+
+}
+
+catch(error){
+
+console.log(error);
+
+alert(
+"Error submitting payment"
+);
+
+}
 
 
 });
