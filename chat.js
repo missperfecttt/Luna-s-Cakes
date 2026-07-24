28e070b6d930b2d224bea1fd19b51c768da5454f
missperfecttt <abigailmekonnen70@gmail.com>
2026-07-24 16:13:14 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,17 @@
+const chatButton = document.getElementById("chatButton");
 
+const chatWindow = document.getElementById("chatWindow");
+
+chatButton.onclick = () => {
+
+if(chatWindow.style.display==="block"){
+
+chatWindow.style.display="none";
+
+}else{
+
+chatWindow.style.display="block";
+
+}
+
+};
