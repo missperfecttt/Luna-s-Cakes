66ec1e2ecdffdb2231f716e9712defed4f63614f
missperfecttt <abigailmekonnen70@gmail.com>
2026-07-25 21:11:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const cloudinaryUrl = "https://api.cloudinary.com/v1_1/xpzpo4yy/image/upload";
 const cloudinaryPreset = "lunascakes_upload";
 
 // Replace with your current Google Apps Script Exec Web App URL
-const scriptURL = "https://script.google.com/macros/s/AKfycbyMrtUt0RWBALZReV31GzwGQvdKvCBb2bOs_yLGA9f34A9R8nbSZcgUzwAkQs2y4TnF3Q/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbw0IbnBuVql2EMe5DAqukZqRs9yZPqkQ4SFJ6PZdHO8EFO-cMBgdm3Ms0JpbqzduDIyPQ/exec";
 
 // ================= ORDER FORM SUBMIT =================
 if (orderForm) {
