7f0b002b4e1e3720230c35c85a597f46e40decb7
missperfecttt <abigailmekonnen70@gmail.com>
2026-07-26 16:36:27 +0300

    Update payment.js

--- payment.js
+++ payment.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const paymentDetails = document.getElementById("paymentDetails");
   const receiptFileInput = document.getElementById("paymentScreenshot") || document.getElementById("receiptImage");
 
-  const scriptURL       = "https://script.google.com/macros/s/AKfycby93W0w74ljLtD7y3SoL9eOuV4cb1BuZzbYjddkz5TcTa1G0sdJKdtaatbwdZXyPA4e-Q/exec";
+  const scriptURL       = "https://script.google.com/macros/s/AKfycbxfhaPVfRMZz4Wu17uQps-Sjrq64lAiXveFuEHfV34q2cGNOKAWSBR6vJcedg44mq86/exec";
   const cloudinaryUrl   = "https://api.cloudinary.com/v1_1/xpzpo4yy/image/upload";
   const cloudinaryPreset = "lunascakes_upload";
 
